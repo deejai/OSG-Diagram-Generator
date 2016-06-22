56a7cfb73fe96008b8cf78e135d229f33c9cc9f4
David <deejai@live.com>
2016-06-22 14:03:59 -0400

    Added debug mode and mostly finished addendum implementation

--- diagram.py
+++ diagram.py
@@ -1,10 +1,12 @@
 import pydotplus as pydot
 import glob
 
+DEBUG = True
+
 class NodeDiagram():
 
     # Fill colors for given node types
-    nodeColors = {
+    node_colors = {
     "root"              : "white",
     "LOD"               : "orange",
     "PagedLOD"          : "yellow",
@@ -18,82 +20,93 @@ class NodeDiagram():
     def __init__(self, name, tag=""):
         self.name = name
         self.tag = tag
-        self.nodeID = 0
+        self.node_id = 0
         self.generated = False
         self.file = open("structure_%s.txt" % name, "r")
         self.graph = pydot.Dot(graph_type = "graph")
 
     def __getLastTag(self):
-        return str(self.nodeID) + self.tag
+        return str(self.node_id) + self.tag
 
     def __addChild(self, parentID, childID):
-        print("addChild(%d, %d)" % (parentID, childID), end="" )
-        if parentID > self.nodeID:
-            print(" ERROR\nInvalid parentID: %d. Current nodeID: %d" %(parentID, self.nodeID) )
+        if DEBUG: print("addChild(%d, %d)" % (parentID, childID), end="" )
+        if parentID > self.node_id:
+            if DEBUG: print(" ERROR\nInvalid parentID: %d. Current node_id: %d"
+                                           % (parentID, self.node_id) )
             exit()
         else:
             self.graph.add_edge(pydot.Edge(str(parentID), str(childID)))
-            print(" Complete.")
+            if DEBUG: print(" Complete.")
 
     def __createNode(self, nodeType, addendum=""):
-        print("createNode(%s) (nodeID: " % nodeType, end="")
-        if nodeType not in self.nodeColors:
-            print("ERROR)\nInvalid nodeType: %s" % nodeType)
+        if DEBUG: print("createNode(%s) (node_id: " % nodeType, end="")
+        if nodeType not in self.node_colors:
+            if DEBUG: print("ERROR)\nInvalid nodeType: %s" % nodeType)
             exit()
         else:
-            self.nodeID += 1
-            print("%s)" % self.__getLastTag())
+            self.node_id += 1
+            if DEBUG: print("%s, addednum: %s)" % (self.__getLastTag(), addendum) )
             if nodeType == "root":
                 label = self.name
             else:
                 label = nodeType + addendum
-            self.graph.add_node(pydot.Node(str(self.nodeID), label=label, style="filled",
-                                                             fillcolor=self.nodeColors[nodeType]))
+            self.graph.add_node(pydot.Node(str(self.node_id), label=label, style="filled",
+                                                              fillcolor=self.node_colors[nodeType]))
 
     def __attachEllipsis(self, nodeType, parentID):
-        # Add indicator that multiple nodes of the given type can exist
-        ID = str(self.nodeID) + "*"
+        # Add indicator that multiple nodes of the given type likely exist
+        # Creates a node, but doesn't increment self.node_id
+        ID = str(self.node_id) + '*'
         self.graph.add_node(pydot.Node(ID, label="...", style="filled", 
-                                           fillcolor=self.nodeColors[nodeType]))
+                                           fillcolor=self.node_colors[nodeType]))
         self.graph.add_edge(pydot.Edge(str(parentID), ID))
         
     def generateGraph(self):
+        # A graph can only be generated once per NodeDiagram
         if self.generated:
-            print("Graph has already been generated")
+            if DEBUG: print("Graph has already been generated")
             return
         else:
-            print(self.name)
+            if DEBUG: print('\n' + self.name)
             self.__createNode("root")
             self.__recursiveGenerate(None, 0)
             self.generated = True
 
     def __recursiveGenerate(self, parentID, depth):
         # parentID can be None or int, so print its statement is separate
-        print("recursiveGenerate(", end="")
-        print(parentID, end="")
-        print(", %d)" % depth )
+        if DEBUG: print("recursiveGenerate(", end="")
+        if DEBUG: print(parentID, end="")
+        if DEBUG: print(", %d)" % depth )
 
-        line = self.file.readline().rstrip()
-        nodeType = line.strip().replace("*", "")
-        while line:
-            lvl = line.count(' ')
-            if lvl >= depth :
-                print("lvl(%d) >= depth(%d)" %(lvl, depth) )
-                self.__createNode( nodeType )
-                if parentID is not None:
-                    self.__addChild(parentID, self.nodeID)
+        while True:
+            line = self.file.readline().rstrip()
+            if not line: break
+            print(line)
 
-                    if( line[-1:] == "*" ):
-                        print(" |--> multi")
-                        self.__attachEllipsis( nodeType, parentID )
-                line = self.__recursiveGenerate(self.nodeID, lvl+1)
+            if ':' in line:
+                addendum = line.split(':')[1] + "\n"
+                print(addendum)
             else:
-                break
+                addendum = ""
+
+            nodeType = line.strip().split(':')[0].replace('*', "")
+            level = line.count(' ')
+            if level < depth: break
+
+            self.__createNode( nodeType, addendum )
+            if parentID is not None:
+                self.__addChild(parentID, self.node_id)
+
+                if( line[-1:] == '*' ):
+                    if DEBUG: print(" |--> multi")
+                    self.__attachEllipsis( nodeType, parentID )
+            line = self.__recursiveGenerate(self.node_id, level+1)
+            
         return line
 
     def generateFile(self):
         if not self.generated:
-            print("PNG file cannot be generated until the graph is generated")
+            if DEBUG: print("PNG file cannot be generated until the graph is generated")
         else:
             self.graph.write_png(self.name + ".png")
 
